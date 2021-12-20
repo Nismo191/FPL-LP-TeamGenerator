6cf11115b1997c40fdd75404e9d122609376adba
Nismo191 <nismo191@googlemail.com>
2021-12-20 17:27:19 +0000

    Added fixutre constraint

--- main.py
+++ main.py
@@ -4,12 +4,26 @@ import numpy as np
 import pandas as pd
 from pulp.apis.coin_api import PULP_CBC_CMD
 
+pd.set_option('display.max_column', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_seq_items', None)
+pd.set_option('display.max_colwidth', 500)
+pd.set_option('expand_frame_repr', True)
 
-def generate_team(budget, owner_theshold, form_theshold, sub_multiplier, excluded_clubs):
+def generate_team(budget, owner_theshold, form_theshold, fixture_threshold, sub_multiplier, excluded_clubs, game_week):
 
     df = pd.read_csv("https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2021-22/players_raw.csv")
+    df_fixtures = pd.read_csv("https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2021-22/fixtures.csv")
 
-    
+    for x in df_fixtures.index:
+        if df_fixtures.loc[x, "event"] != game_week:
+            df_fixtures.drop(x, inplace=True)
+
+    df_fixtures.reset_index(drop=True, inplace=True)
+
+    # df_fixtures.rename(columns={"team_h": "team"}, inplace=True)
+    # # df_fixtures.rename(columns={"team_a": "team"}, inplace=True)
+
     for x in df.index:
         if df.loc[x, "chance_of_playing_this_round"] == "None":
             df.loc[x, "chance_of_playing_this_round"] = 100
@@ -20,20 +34,43 @@ def generate_team(budget, owner_theshold, form_theshold, sub_multiplier, exclude
 
     df.reset_index(drop=True, inplace=True)
 
+    df = pd.merge(df, df_fixtures, left_on="team", right_on="team_a", how="left")
+    df = pd.merge(df, df_fixtures, left_on="team", right_on="team_h", how="left")
+
+    df = df.assign(**{"team_h_y": df.team_h_y.fillna(df.team_h_x)})
+    df = df.assign(**{"team_a_y": df.team_a_y.fillna(df.team_a_x)})
+    df = df.assign(**{"team_h_difficulty_y": df.team_h_difficulty_y.fillna(df.team_h_difficulty_x)})
+    df = df.assign(**{"team_a_difficulty_y": df.team_a_difficulty_y.fillna(df.team_a_difficulty_x)})
+
+
+    fixture_difficulty = []
+    for x in df.index:
+        if df.loc[x, "team_h_y"] == float(df.loc[x, "team"]):
+            fixture_difficulty.append(int(df.loc[x, "team_h_difficulty_y"]))
+        else:
+            fixture_difficulty.append(int(df.loc[x, "team_a_difficulty_y"]))
+
+    df["fixture_difficulty"] = fixture_difficulty
+
+    # print(df[["team", "team_h_y", "team_a_y", "team_h_difficulty_y", "team_a_difficulty_y", "fixture_difficulty"]])
+    # df.to_csv("debug.csv")
+
     expected_scores = df["total_points"]
     prices = df["now_cost"] / 10
     positions = df["element_type"]
+    team_id = df["team"]
     clubs = df["team_code"]
     name = df["web_name"]
     selected_percentage = df["selected_by_percent"]
     form = df["form"]
     chance_of_playing = df["chance_of_playing_this_round"]
+    fixture_difficulty = df["fixture_difficulty"]
 
     num_players = len(expected_scores)
     total_budget = budget
     sub_factor = sub_multiplier
 
-    print("Included clubs: ")
+    print("Excluded clubs: ")
     print(np.unique(clubs))
 
     model = p.LpProblem("Maximize Points", p.LpMaximize)
@@ -93,9 +130,13 @@ def generate_team(budget, owner_theshold, form_theshold, sub_multiplier, exclude
     # Playing Chance constraint
     model += sum(decisions[i] for i in range(num_players) if int(chance_of_playing[i]) >= 100) == 11
 
+    # Fixture constraint
+    model += sum(decisions[i] for i in range(num_players) if fixture_difficulty[i] <= fixture_threshold) == 11
+
     # club constraint
     for club_id in np.unique(clubs):
         model += sum(decisions[i] + sub_decisions[i] for i in range(num_players) if clubs[i] == club_id) <= 3  # max 3 players
+
 
     model += sum(decisions) == 11  # total team size
     model += sum(captain_decisions) == 1  # 1 captain
@@ -123,7 +164,7 @@ def generate_team(budget, owner_theshold, form_theshold, sub_multiplier, exclude
         for i in range(df.shape[0]):
             if decisions[i].value() != 0:
                 if df["element_type"][i] == j:
-                    print("**{}** Points = {}, Price = {}, Owned % = {}, Form = {}, Play Chance = {}".format(df["web_name"][i], df["total_points"][i], df["now_cost"][i], df["selected_by_percent"][i], df["form"][i], df["chance_of_playing_next_round"][i]))
+                    print("**{}** Points = {}, Price = {}, Owned % = {}, Form = {}, Play Chance = {}, Fixture_difficulty = {}".format(df["web_name"][i], df["total_points"][i], df["now_cost"][i], df["selected_by_percent"][i], df["form"][i], df["chance_of_playing_next_round"][i], df["fixture_difficulty"][i]))
                     price += df["now_cost"][i]
                     points += df["total_points"][i]
     print("Total Price: {}".format(price/10))
@@ -148,8 +189,10 @@ def generate_team(budget, owner_theshold, form_theshold, sub_multiplier, exclude
 if __name__ == "__main__":
     #[ 1  2  3  4  6  7  8 11 13 14 20 21 31 36 39 43 45 57 90 94]
     #[ 2        4  6  7  8 11 13    20 21 31 36    43 45 57 90 94]
-    excluded_teams = [94, 36, 31, 11, 13, 1, 45, 20, 57, 21]
-    generate_team(100.6, 100, 3, 0.1, excluded_teams)
+    # excluded_teams = [94, 36, 31, 11, 13, 1, 45, 20, 57, 21, 7, 90]
+    excluded_teams = []
+    game_week = 19.0
+    generate_team(100.6, 100, 3, 2, 0.1, excluded_teams, game_week)
 
 
 # GW16
